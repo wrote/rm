edff78f34750cfb21eebe6338672c2157a2305b9
Anton <anton@adc.sh>
2018-09-15 01:27:06 +0300

    build

--- build/index.js
+++ build/index.js
@@ -1,26 +1,52 @@
-const { debuglog } = require('util');
+const { unlink, rmdir, lstat } = require('fs');
+let readDirStructure = require('@wrote/read-dir-structure'); if (readDirStructure && readDirStructure.__esModule) readDirStructure = readDirStructure.default;
+let makePromise = require('makepromise'); if (makePromise && makePromise.__esModule) makePromise = makePromise.default;
+const { join } = require('path');
 
-const LOG = debuglog('@wrote/rm')
-
-/**
- * A package to remove files and directories.
- * @param {Config} config Options for the program.
- * @param {boolean} config.shouldRun A boolean option.
- */
-               async function rm(config) {
-  const {
-    type,
-  } = config
-  LOG('@wrote/rm called with %s', type)
-  return type
+const removeFile = async (path) => {
+  await makePromise(unlink, path)
 }
 
-/* documentary types/index.xml */
 /**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} shouldRun A boolean option.
+ * Removes files and directories.
+ * @param {string} path Path to remove.
  */
+const removeDir = async (path) => {
+  const { content } = await readDirStructure(path)
+  const files = Object.keys(content).filter((k) => {
+    const { type } = content[k]
+    if (type == 'File') return true
+  })
+  const dirs = Object.keys(content).filter((k) => {
+    const { type } = content[k]
+    if (type == 'Directory') return true
+  })
+  const filesFullPaths = files.map(file => join(path, file))
+  await Promise.all(
+    filesFullPaths.map(removeFile)
+  )
+  const dirsFullPaths = dirs.map(dir => join(path, dir))
+  await Promise.all(
+    dirsFullPaths.map(removeDir)
+  )
+  await makePromise(rmdir, path)
+}
 
+/**
+ * Removes a path from the filesystem.
+ * @param {string} path Path to the file or directory to remove.
+ */
+const rm = async (path) => {
+  const s = await makePromise(lstat, path)
+  if (s.isDirectory()) {
+    await removeDir(path)
+  } else {
+    await removeFile(path)
+  }
+}
 
-module.exports = rm
+module.exports=rm
+
+/* documentary types/index.xml */
+
 //# sourceMappingURL=index.js.map
